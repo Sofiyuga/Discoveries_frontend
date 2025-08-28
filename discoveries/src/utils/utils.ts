19499b6b9ad37a9c12f70b2c582be54e971814e2
Sofiyuga <114177738+Sofiyuga@users.noreply.github.com>
2025-08-28 11:38:24 +0300

    Delete discoveries directory

--- discoveries/src/utils/utils.ts
+++
@@ -1,9 +0,0 @@
-export const pluralVerify = (value) => {
-    if (value == -1) {
-        return "Нет"
-    } else if (value == 0) {
-        return "Отсутствует"
-    }
-
-    return "Присутствует"
-}
